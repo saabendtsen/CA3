ad7f892d9ce4f862814c5d2310b285a43aa764b3
Younes Karim <younes-e@hotmail.com>
2021-11-23 13:47:17 +0100

    changes

---
+++ src/main/java/datamappers/MovieMapper.java
@@ -0,0 +1,18 @@
+package datamappers;
+
+import dtos.ImdbMovieDTO;
+
+import java.util.List;
+
+public class MovieMapper {
+
+
+    private List<ImdbMovieDTO> getMovie(ImdbMovieDTO imdbMovieDTO){
+        for ( o : ) {
+
+        }
+
+        return null;
+    }
+
+}
