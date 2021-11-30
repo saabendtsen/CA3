298bc76550981e53d99f540ebe9d120e0733ca0c
Younes Karim <younes-e@hotmail.com>
2021-11-30 15:56:48 +0100

    test upload

--- src/main/java/security/HttpClient.java
+++ src/main/java/security/HttpClient.java
@@ -26,14 +26,11 @@ public class HttpClient {
 
     public String sendGet(String search, Boolean isID) throws Exception {
         String res = gson.toJson("NotFound : Movie not Found!");
-            HttpGet request;
+        HttpGet request;
 
-        if(isID){
-            request = new HttpGet("https://movie-database-imdb-alternative.p.rapidapi.com/?r=json&i=" + search );
-
-
+        if (isID) {
+            request = new HttpGet("https://movie-database-imdb-alternative.p.rapidapi.com/?r=json&i=" + search);
         } else {
-
             request = new HttpGet("https://utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com/lookup?term=%22" + search + "&country=us");
         }
         // request headers
@@ -41,10 +38,10 @@ public class HttpClient {
         try (CloseableHttpResponse response = httpClient.execute(request)) {
             HttpEntity entity = response.getEntity();
             if (entity != null) {
-                res =  EntityUtils.toString(entity);
+                res = EntityUtils.toString(entity);
             }
         }
-            return res;
+        return res;
     }
 
 }
